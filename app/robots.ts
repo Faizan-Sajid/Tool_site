35452908781554768827894831517a5b75ce7c96
faizan sajid <165155018+Faizan-Sajid@users.noreply.github.com>
2026-05-08 18:03:14 +0500

    commit

--- app/robots.ts
+++ app/robots.ts
@@ -4,9 +4,15 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
+      allow: [
+        '/',
+        '/_next/static/', // Ye line CSS aur JS files ko allow karegi
+      ],
+      disallow: [
+        '/api/', 
+        '/_next/', // Ye baqi internal folders ko block rakhega
+      ],
     },
-    sitemap: 'https://www.quickcalcs.app/sitemap.xml', 
+    sitemap: 'https://www.quickcalcs.app/sitemap.xml',
   };
 }
